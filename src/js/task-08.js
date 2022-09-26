d62eca6a856c6bc2602e8419df985babb3ae35da
serge-belkin <cer_fi@yahoo.com>
2022-09-26 10:09:42 +0300

    adds 8 task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -10,3 +10,23 @@
 
 
 "use strict";
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const {
+    elements: { email, password },
+  } = event.currentTarget
+
+  if (email.value === '' || password.value === '') {
+    return alert('Будь ласка заповніть всі поля!')
+  }
+
+  const userValues = { email: email.value, password: password.value };
+
+  console.log(userValues);
+  event.currentTarget.reset();
+}
