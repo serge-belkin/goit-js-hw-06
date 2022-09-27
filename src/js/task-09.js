1615509da5688584dd5ac97c9421fad787c9b3c8
serge-belkin <cer_fi@yahoo.com>
2022-09-27 10:22:42 +0300

    adds 9-10 tasks

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,22 @@
+// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку
+//  на button.change - color і виводить значення кольору в span.color.
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeBtn = document.querySelector('button.change-color');
+
+const body = document.querySelector('body');
+
+const colorName = document.querySelector('span.color');
+
+changeBtn.addEventListener('click', event => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorName.textContent = color;
+})
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,52 @@
+// Напиши скрипт створення і очищення колекції елементів. 
+// Користувач вводить кількість елементів в input і натискає кнопку Створити,
+//   після чого рендериться колекція.Натисненням на кнопку Очистити,
+//     колекція елементів очищається.
+
+// Створи функцію createBoxes(amount), яка приймає один параметр - число. 
+// Функція створює стільки < div >, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX. 
+// Використовуй готову функцію getRandomHexColor для отримання кольору.
+
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes,
+//   у такий спосіб видаляючи всі створені елементи.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const inputNum = document.querySelector('#controls>input');
+const boxes = document.querySelector('#boxes');
+
+const createBox = amount => {
+  const elements = [];
+  for (let index = 0; index < amount; index++) {
+    const div = document.createElement('div');
+    div.style.background = getRandomHexColor();
+    div.style.width = `${30 + 10 * index}px`;
+    div.style.height = `${30 + 10 * index}px`;
+    
+    elements.push(div);
+  }
+  return elements;
+}
+
+const destroyBox = () => {
+  boxes.innerHTML = ''
+};
+
+createBtn.addEventListener('click', () => {
+  let newBoxes = createBox(inputNum.value);
+  boxes.append(...newBoxes);
+})
+
+console.log(inputNum.value);
+
+destroyBtn.addEventListener('click', () => {
+  destroyBox.call()
+})
